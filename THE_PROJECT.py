7b6ead3000a638cce354e46e4d6b0281b19e2a48
Galaxy1T-Codes <166913731+Galaxy1T-Codes@users.noreply.github.com>
2024-04-26 11:33:38 +0200

    Add files via upload

---
+++ THE_PROJECT.py
@@ -0,0 +1,45 @@
+import freetemps
+
+print("Welcome! To The...")
+print("THE_PROJECT")
+print("                ____                                                                     ")
+print("_______________/Main\____________________________________________________________________")
+## __Func__
+def problems():(
+print("Have A Great Day Dont Be Sad Have Fun!")
+)
+def prt_first_art():(
+print("""
+
+------------------------------------------------------------ /
+  Linux                                                     /
+  Windows                                                  /
+  MacOs                                                   /
+                                                         /
+                                                        /
+                                                       /
+                                                      /
+-----------------------------------------------------/
+
+""")
+)
+def prt_second_art():(
+print("""
+
+  -------------------------------------
+ | Linux                               |
+ | Windows                             |
+ | MacOs                               |
+ |                                     |
+ |                                     |
+ |                                     |
+ |                                     |
+ |                                     |
+ |                                     |
+  -------------------------------------
+
+""")
+)
+## __Code
+prt_first_art()
+prt_second_art()
---
+++ license.md
@@ -0,0 +1,6 @@
+**Galaxy1T License Agreement**
+    terms and conditions:
+        1. You Cannot Use This For Malicious Purpose
+        2. Its Free and Open-Source
+        3. You Can Use This For Fun And etc...
+        4. Byeeeee Have Fun!!
